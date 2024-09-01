87a691105bb6970441a16990f2d9fbc044d8c32a
georgia-iris <79837147+georgia-iris@users.noreply.github.com>
2024-09-01 21:46:23 +0300

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,201 @@
+<?php
+/*<!--/.blog-section-->
+<section class="blog-section bg-grey padding">
+        <div class="container">
+            <div class="section-heading text-center mb-40 wow fade-in-bottom" data-wow-delay="200ms">
+                <h4><span></span>Recent Posts!</h4>
+                <h2>News And Insights!</h2>
+                <p>We successfully cope with tasks of varying complexity, provide long-term <br>guarantees and regularly master new technologies.</p>
+            </div>
+            <!--/.section-heading-->
+            <div class="row grid-post">
+                <div class="col-lg-4 col-md-6 padding-15">
+                    <div class="post-card">
+                        <div class="post-thumb">
+                            <img src="assets/img/post-1.jpg" alt="post">
+                            <a href="blog-details.html" class="post-category">Business</a>
+                        </div>
+                        <div class="post-content-wrap">
+                            <ul class="post-meta">
+                                <li><i class="las la-calendar"></i>Jan 01 2022</li>
+                                <li><i class="las la-user"></i>Elliot Alderson</li>
+                            </ul>
+                            <div class="post-content">
+                                <h3>
+                                    <a href="blog-details.html" class="hover">How to start initiating an startup in few days.</a>
+                                </h3>
+                                <p>Financial experts support or help you to to find out which way you can raise your funds more...</p>
+                                <a href="blog-details.html" class="read-more">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 padding-15">
+                    <div class="post-card">
+                        <div class="post-thumb">
+                            <img src="assets/img/post-2.jpg" alt="post">
+                            <a href="blog-details.html" class="post-category">Startup</a>
+                        </div>
+                        <div class="post-content-wrap">
+                            <ul class="post-meta">
+                                <li><i class="las la-calendar"></i>Jan 01 2022</li>
+                                <li><i class="las la-user"></i>Elliot Alderson</li>
+                            </ul>
+                            <div class="post-content">
+                                <h3>
+                                    <a href="blog-details.html">Financial experts support help you to find out.</a>
+                                </h3>
+                                <p>Financial experts support or help you to to find out which way you can raise your funds more...</p>
+                                <a href="blog-details.html" class="read-more">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 padding-15">
+                    <div class="post-card">
+                        <div class="post-thumb">
+                            <img src="assets/img/post-3.jpg" alt="post">
+                            <a href="blog-details.html" class="post-category">Finance</a>
+                        </div>
+                        <div class="post-content-wrap">
+                            <ul class="post-meta">
+                                <li><i class="las la-calendar"></i>Jan 01 2022</li>
+                                <li><i class="las la-user"></i>Elliot Alderson</li>
+                            </ul>
+                            <div class="post-content">
+                                <h3>
+                                    <a href="blog-details.html">Innovative business all over the world.</a>
+                                </h3>
+                                <p>Financial experts support or help you to to find out which way you can raise your funds more...</p>
+                                <a href="blog-details.html" class="read-more">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--/.blog-section-->*/
+?>
+<footer class="footer-section">
+        <div class="footer-top-wrap">
+            <div class="container">
+                <div class="footer-top">
+                    <div class="row align-items-end">
+                        <div class="col-lg-8">
+                            <div class="brand">
+                                <a class="footer-logo" href="index.html"><img src="assets/img/logo-light.png" alt="logo"></a>
+                                <p>We successfully cope with tasks of varying complexity, provide long-term guarantees and regularly master new technologies.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="footer-call">
+                                <i class="las la-phone-volume"></i>
+                                <p><span>Call For Taxi</span><a href="tel:5267214392">5267-214-392</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--/.footer-top-wrap-->
+        <div class="footer-mid-wrap">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3 col-md-6 sm-padding">
+                        <div class="footer-item">
+                            <div class="widget-title">
+                                <h3>Working Hours</h3>
+                            </div>
+                            <ul class="footer-contact">
+                                <li><span>Monday - Friday:</span>9.00am To 8.00pm</li>
+                                <li><span>Saturday:</span>10.00am To 7.30pm</li>
+                                <li><span>Sunday:</span>Close Day!</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 sm-padding">
+                        <div class="footer-item footer-list">
+                            <div class="widget-title">
+                                <h3>Usefull Links</h3>
+                            </div>
+                            <ul class="footer-links">
+                                <li><a href="book-taxi.html">Taxi Booking</a></li>
+                                <li><a href="faqs.html">Help Center</a></li>
+                                <li><a href="about-us.html">Privacy and Policy</a></li>
+                                <li><a href="about-company.html">Terms of Use</a></li>
+                                <li><a href="contact.html">Contact Us</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 sm-padding">
+                        <div class="footer-item">
+                            <div class="widget-title">
+                                <h3>Head Office</h3>
+                            </div>
+                            <ul class="footer-contact">
+                                <li><span>Location:</span>153 Williamson Plaza, Maggieberg, MT 09514</li>
+                                <li><span>Join Us:</span>Info@YourGmail24.com</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 sm-padding">
+                        <div class="footer-item subscribe-wrap">
+                            <div class="widget-title">
+                                <h3>Newsletter Signup</h3>
+                            </div>
+                            <form action="#" class="subscribe-form">
+                                <input class="form-control" type="email" name="email" placeholder="Your Email" required="">
+                                <input type="hidden" name="action" value="mailchimpsubscribe">
+                                <button class="submit">Subscribe Now</button>
+                                <div class="clearfix"></div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="running-taxi">
+                <div class="taxi"></div>
+                <div class="taxi-2"></div>
+                <div class="taxi-3"></div>
+            </div>
+        </div>
+        <!--/.footer-mid-wrap-->
+        <div class="copyright-wrap">
+            <div class="container">
+                <p>© <span id="currentYear"></span> DynamicLayers All Rights Reserved.</p>
+            </div>
+        </div>
+        <!--/.copyright-wrap-->
+    </footer>
+    <!--/.footer-section-->
+
+    <div id="scrollup">
+        <button id="scroll-top" class="scroll-to-top">
+            <i class="las la-arrow-up"></i>
+        </button>
+    </div>
+    <!--scrollup-->
+
+    <div class="dl-cursor">
+        <div class="cursor-icon-holder"><i class="las la-times"></i></div>
+    </div>
+    <!--/.dl-cursor-->
+
+    <!--jQuery Lib-->
+    <script src="assets/js/vendor/jquary-3.6.0.min.js"></script>
+    <script src="assets/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    <script src="assets/js/vendor/jquery.ajaxchimp.min.js"></script>
+    <script src="assets/js/vendor/bootstrap.min.js"></script>
+    <script src="assets/js/vendor/popper.min.js"></script>
+    <script src="assets/js/vendor/swiper.min.js"></script>
+    <script src="assets/js/vendor/jquery.datetimepicker.full.js"></script>
+    <script src="assets/js/vendor/jquery.nice-select.min.js"></script>
+    <script src="assets/js/vendor/venobox.min.js"></script>
+    <script src="assets/js/vendor/smooth-scroll.js"></script>
+    <script src="assets/js/vendor/wow.min.js"></script>
+    <script src="assets/js/book-ride.js"></script>
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
---
+++ includes/header.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="en">
+<!--<![endif]-->
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Ridek Online Taxi Booking HTML5 Template">
+    <meta name="author" content="DynamicLayers">
+
+    <title>Ridek - Online Taxi Booking Service HTML5 Template</title>
+
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <link rel="stylesheet" href="assets/css/fontawesome.min.css">
+    <link rel="stylesheet" href="assets/css/line-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/keyframe-animation.css">
+    <link rel="stylesheet" href="assets/css/jquery.datetimepicker.min.css">
+    <link rel="stylesheet" href="assets/css/nice-select.css">
+    <link rel="stylesheet" href="assets/css/venobox.min.css">
+    <link rel="stylesheet" href="assets/css/swiper.min.css">
+    <link rel="stylesheet" href="assets/css/elements.css">
+    <link rel="stylesheet" href="assets/css/header.css">
+    <link rel="stylesheet" href="assets/css/slider.css">
+    <link rel="stylesheet" href="assets/css/blog.css">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
+</head>
+
+<body>
+    <!--[if lt IE 8]>
+	<p class="browserupgrade">
+        You are using an <strong>outdated</strong> browser. Please
+        <a href="http://browsehappy.com/">upgrade your browser</a> to improve
+        your experience.
+	</p><![endif]-->
+
+    <div class="site-preloader">
+        <div class="car">
+            <div class="strike"></div>
+            <div class="strike strike2"></div>
+            <div class="strike strike3"></div>
+            <div class="strike strike4"></div>
+            <div class="strike strike5"></div>
+            <div class="car-detail spoiler"></div>
+            <div class="car-detail back"></div>
+            <div class="car-detail center"></div>
+            <div class="car-detail center1"></div>
+            <div class="car-detail front"></div>
+            <div class="car-detail wheel"></div>
+            <div class="car-detail wheel wheel2"></div>
+        </div>
+    </div>
+    <!--/.site-preloader-->
+
+    <header class="main-header">
+        <div class="top-header">
+            <div class="container">
+                <div class="top-header-wrap">
+                    <div class="top-left">
+                        <p>Reliable Taxi Service & Transport Solutions!</p>
+                    </div>
+                    <div class="top-right">
+                        <ul class="top-header-nav">
+                            <li><a href="faqs.php">Help</a></li>
+                            <li><a href="contact.php">Support</a></li>
+                            <li><a href="faqs.php">FAQ</a></li>
+                        </ul>
+                        <ul class="header-social-share">
+                            <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                            <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                            <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                            <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--/.top-header-->
+        <div class="mid-header">
+            <div class="container">
+                <div class="mid-header-wrap">
+                    <div class="site-logo">
+                        <a href="index.php"><img src="assets/img/logo-dark.png" alt="Logo"></a>
+                    </div>
+                    <!--/.site-logo-->
+                    <ul class="header-info">
+                        <li>
+                            <div class="header-info-icon">
+                                <i class="las la-phone-volume"></i>
+                            </div>
+                            <div class="header-info-text">
+                                <h3><span>Call us now</span><a href="tel:5267214392">5267-214-392</a></h3>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="header-info-icon">
+                                <i class="las la-envelope-open"></i>
+                            </div>
+                            <div class="header-info-text">
+                                <h3><span>Email now</span>Info.ridek@mail.com</h3>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="header-info-icon">
+                                <i class="las la-map-marked-alt"></i>
+                            </div>
+                            <div class="header-info-text">
+                                <h3><span>Halk Street</span>New York, USA - 2386</h3>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!--/.mid-header-->
+        <div class="nav-menu-wrapper">
+            <div class="container">
+                <div class="nav-menu-inner">
+                    <div class="site-logo">
+                        <a href="index.php"><img src="assets/img/logo-dark.png" alt="Logo"></a>
+                    </div>
+                    <!--/.site-logo-->
+                    <div class="header-menu-wrap">
+                        <ul class="nav-menu">
+                            <li><a href="index.php">Home</a></li>
+                                                            <li class="dropdown_menu">
+                                <a href="about-us.php">Company</a>
+                                <ul>
+                                    <li><a href="about-us.php">About Us</a></li>
+                                    <li><a href="about-company.php">About Company</a></li>
+                                    <li><a href="our-services.php">Our Services</a></li>
+                                    <li><a href="service-details.php">Services Details</a></li>
+                                    <li><a href="book-taxi.php">Book a Ride</a></li>
+                                </ul>
+                            </li>
+                            <li class="dropdown_menu">
+                                <a href="our-taxi.php">Our Taxi</a>
+                                <ul>
+                                    <li><a href="our-taxi.php">Taxi Lists</a></li>
+                                    <li><a href="taxi-details.php">Taxi Details</a></li>
+                                </ul>
+                            </li>
+                            <li class="dropdown_menu">
+                                <a href="#">Pages</a>
+                                <ul>
+                                    <li><a href="our-drivers.php">Our Drivers</a></li>
+                                    <li><a href="driver-details.php">Driver Details</a></li>
+                                    <li><a href="testimonials.php">Customer Reviews</a></li>
+                                    <li><a href="faqs.php">Help &amp; Faq's</a></li>
+                                    <li><a href="404.php">404 Error</a></li>
+                                </ul>
+                            </li>
+                           <!-- <li class="dropdown_menu">
+                                <a href="blog-grid.php">Blog</a>
+                                <ul>
+                                    <li><a href="blog-grid.php">Grid Posts</a></li>
+                                    <li><a href="blog-classic.php">Classic Posts</a></li>
+                                    <li><a href="blog-details.php">Blog Details</a></li>
+                                </ul>
+                            </li> -->
+                            <li><a href="contact.php">Contact</a></li>
+                        </ul>
+                    </div>
+                    <div class="menu-right-item">
+                        <div class="search-icon dl-search-icon">
+                            <i class="las la-search"></i>
+                        </div>
+                        <div class="sidebox-icon dl-sidebox-icon">
+                            <i class="las la-bars"></i>
+                        </div>
+                        <a href="book-taxi.php" class="menu-btn">Book a Taxi</a>
+                    </div>
+                    <div class="mobile-menu-icon">
+                        <div class="burger-menu">
+                            <div class="line-menu line-half first-line"></div>
+                            <div class="line-menu"></div>
+                            <div class="line-menu line-half last-line"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--/.nav-menu-->
+    </header>
+    <!--/.main-header-->
+    <div id="popup-search-box">
+        <div class="box-inner-wrap d-flex align-items-center">
+            <form id="form" action="#" method="get" role="search">
+                <input id="popup-search" type="text" name="s" placeholder="Type keywords here...">
+                <button id="popup-search-button" type="submit" name="submit">
+                    <i class="las la-search"></i>
+                </button>
+            </form>
+            <div class="search-close"><i class="las la-times"></i></div>
+        </div>
+    </div>
+    <!--/.popupsearch-box-->
+    <div id="searchbox-overlay"></div>
+
+    <div id="popup-sidebox" class="popup-sidebox">
+        <div class="sidebox-content">
+            <div class="site-logo">
+                <a href="index.php"><img src="assets/img/logo-light.png" alt="logo"></a>
+            </div>
+            <p>Everything your taxi business needs is already here! Ridek, a theme
+                made for taxi service companies.</p>
+            <ul class="sidebox-list">
+                <li class="call"><span>Call for ride:</span>5267-214-392</li>
+                <li>
+                    <span>You can find us at:</span>Halk Street New York, USA - 2386
+                </li>
+                <li><span>Email now:</span>Info.ridek@mail.com</li>
+            </ul>
+        </div>
+    </div>
+    <!--/.popup-sidebox-->
